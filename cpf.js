8d87dfe9a5a0d4957384c2066cb98bdf25126f5f
trescapa <163129170+trescapa@users.noreply.github.com>
2025-03-06 07:42:30 -0300

    Update cpf.js

--- cpf.js
+++ cpf.js
@@ -1,15 +1,2 @@
-//criar uma varialvel para armazenar o cpf
-let cpf = 12345678901
-//verificar se a vaiavel tem 11 digitos
-if(cpf.l)
-//separa cada numero do cpf em um item do array(replace, split)
 
-//criar uma variavel chamada somatorio 
-// criar loop para multiplicar os digitos do cpf e somar na variavel soma
-// calcular o resto da divisao por 11 da varaiavel somatorio %
-// criar um codicional para verificar se o figito é valido
-// Função para validar CPF
- 
-git remoto adicionar origem https:github.com/trescapa/atividade.cpf.git
- git branch -M main 
-git push -u origin main
+''
